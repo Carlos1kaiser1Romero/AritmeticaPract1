013dd38709a7b9551b2bc3e64edfd9a2e393d02d
carlos augusto romero orozco <65257318+Carlos1kaiser1Romero@users.noreply.github.com>
2022-11-17 18:49:34 -0400

    Agregar archivos de proyecto.

---
+++ Aritmetica/Program.cs
@@ -0,0 +1,19 @@
+﻿using Antlr4.Runtime;
+using Aritmetica;
+string input = "22*5+5-8/2";
+ICharStream Stream = CharStreams.fromString(input);
+
+aritmeticaLexer lexer = new aritmeticaLexer(Stream);
+
+CommonTokenStream tokens = new CommonTokenStream(lexer);    
+
+aritmeticaParser parser = new aritmeticaParser(tokens);
+
+var tree = parser.aritmetica();
+
+Visita vistado = new Visita();
+
+int resultado =vistado.Visit(tree);
+
+
+
---
+++ Aritmetica/Visita.cs
@@ -0,0 +1,37 @@
+﻿using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aritmetica
+{
+    internal class Visita : aritmeticaBaseVisitor<int>
+    {
+        public override int VisitAritmetica([NotNull] aritmeticaParser.AritmeticaContext context)
+        {
+            Console.WriteLine($"Aritmetica: {context.GetText()}");
+            return base.VisitAritmetica(context);
+        }
+
+        public override int VisitExpresion([NotNull] aritmeticaParser.ExpresionContext context)
+        {
+            Console.WriteLine($"Expresion: {context.GetText()}");
+            return base.VisitExpresion(context);
+        }
+
+        public override int VisitFactor([NotNull] aritmeticaParser.FactorContext context)
+        {
+            Console.WriteLine($"Factor: {context.GetText()}");
+            return base.VisitFactor(context);
+        }
+
+        public override int VisitTermino([NotNull] aritmeticaParser.TerminoContext context)
+        {
+            Console.WriteLine($"Termino: {context.GetText()}");
+            return base.VisitTermino(context);
+        }
+    }
+}
